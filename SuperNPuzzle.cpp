56f3b7e68a05a07091ece9937586330ef0633946
Armando Herrera <anhydrous99@gmail.com>
2019-10-13 18:05:51 -0500

    Format change

--- SuperNPuzzle.cpp
+++ SuperNPuzzle.cpp
@@ -184,9 +184,9 @@ string SuperNPuzzle::depth_first_search() {
         next_state.actions += act;
 
         if (traveled_states.find(next_state.state) == traveled_states.end() &&
-            std::find_if(unexplored.begin(), unexplored.end(),
-                         [&](const SuperState &state) { return state.state == next_state.state; }) ==
-            unexplored.end()) {
+        std::find_if(unexplored.begin(), unexplored.end(), [&](const SuperState &state) {
+          return state.state == next_state.state;
+        }) == unexplored.end()) {
           unexplored.push_back(next_state);
         } // if end
       } // if end
@@ -257,9 +257,9 @@ string SuperNPuzzle::breadth_first_search() {
         next_state.actions += act;
 
         if (traveled_states.find(next_state.state) == traveled_states.end() &&
-            std::find_if(unexplored.begin(), unexplored.end(),
-                         [&](const SuperState &state) { return state.state == next_state.state; }) ==
-            unexplored.end()) {
+        find_if(unexplored.begin(), unexplored.end(), [&](const SuperState &state) {
+          return state.state == next_state.state;
+        }) == unexplored.end()) {
           unexplored.push_back(next_state);
         } // if end
       } // if end
