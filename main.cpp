89a7b8ebd1028bc044c04f3b7f6704e1cddf4141
Armando Herrera <anhydrous99@gmail.com>
2019-10-14 01:19:00 -0500

    Started to create main function and added external memory usage function by David Robert Nadeau

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <random>
+#include <algorithm>
+#include "SuperNPuzzle.h"
+using namespace std;
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  // Create input vectors that are the same as given
+  vector<int> v1 = {0,1,2,4,5,3,7,8,6};
+  vector<int> v2 = {1,2,3,4,6,8,7,5,0};
+  vector<int> v3 = {8,5,0,4,6,3,7,2,1};
+
+  // Create puzzles
+  SuperNPuzzle puzzle1(v1);
+  SuperNPuzzle puzzle2(v2);
+  SuperNPuzzle puzzle3(v3);
+  SuperNPuzzle puzzle4;
+  SuperNPuzzle puzzle5;
+
   return 0;
 }
